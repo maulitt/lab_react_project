5d61e55f87d1517051e5f3f8a636e83421891b11
regina <brf001@campus.mephi.ru>
2020-11-20 13:34:41 +0300

    started my template for news section (options problem hasn't been solved yet though) (pleease help me save me i'm begging)

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,67 @@
 import '../styles/App.css';
 import {A, useRoutes} from 'hookrouter';
 import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+
+
+function Article(props) {
+    Article.PropTypes = {
+        data: Article.PropTypes.shape({
+            author: Article.PropTypes.string.isRequired,
+            text: Article.PropTypes.string.isRequired,
+        })
+    };
+    let author = props.author;
+    let text = props.text;
+    return (
+        <div className={'article'}>
+            <p className={'article_author'}>{author}</p>
+            <p className={'article_text'}>{text}</p>
+        </div>
+    );
+}
+function News() {
+    News.PropTypes = {
+        data: News.PropTypes.array.isRequired
+    };
+    let newsTemplate;
+}
 
 
 
 
+
+
+
+function Test(props) {
+    function alrt() {
+        alert(props.user);
+    }
+    return (
+        <Button color="primary" onClick={alrt}>Show User</Button>
+    )
+}
 
 
 function Main() {
+    function GETCcheck() {
+        fetch('http://127.0.0.1:3000/api/proverka', {
+            method: 'GET',
+        })
+            .then(response => {return response.text();})
+            .then(data => {
+                console.log(data);
+            });
+    }
+
     return (
         <div id="mainy">
             <div className="mainpage">
                 <h1>Main page</h1>
                 <p>Of the app</p>
+                <Test user={'name'}/>
+                <Button color="primary" type="submit" onClick={GETCcheck}>OK</Button>
             </div>
         </div>
     )

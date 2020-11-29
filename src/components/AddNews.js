e36ba8d11a2ec20fd0cd082fd326d789447fee10
regina <brf001@campus.mephi.ru>
2020-11-29 22:25:46 +0300

    now addnews looks priemlemo (kinda like it, though maybe will change later)

--- src/components/AddNews.js
+++ src/components/AddNews.js
@@ -8,41 +8,46 @@ export function AddNews() {
     const [text, setText] = useState();
     function handleSubmit(event) {
         event.preventDefault();
-        alert('success!');
+        alert('С ДНЁМ МАМЫ, МАМА! <3 <3');
     }
     return(
         <form>
-            <div id="mainy">
-                <div className="registration">
+            <div id="add">
+                <div className="addnew">
                     <h1>What's up?</h1>
-                    <p><b>Title</b> <br />
+                    <p><b>Title</b>
                         <label>
                             <input
                                 value={title}
-                                name="name"
+                                name="title"
                                 type="text" size="40"
-                                placeholder={'Title..'}
+                                placeholder={'Give a title to your piece...'}
+                                maxLength={150}
                                 onChange={event => { setTitle(event.target.value) } }
                             />
                         </label>
                     </p>
-                    <p><b>Preview</b><br />
+                    <p><b>Preview</b>
                         <label>
                             <textarea
                                 value={preview}
                                 name="preview"
-                                placeholder={'Preview..'}
+                                placeholder={'Describe it in a few words, don\'t be shy...'}
+                                maxLength={200}
                                 onChange={event => { setPreview(event.target.value) } }
                             />
                         </label>
                     </p>
-
-                </div>
-                <div className="right">
-                    <textarea value={text}
-                              name={'text'}
-                              placeholder={'Your news...'}
-                              onChange={event => {setText(event.target.value)}}> </textarea>
+                    <p><b>Article!</b>
+                        <label>
+                            <textarea
+                                value={text}
+                                name={'text'}
+                                placeholder={'Let\'s go, I\'m ready, what\'s new, what\'s up?'}
+                                onChange={event => {setText(event.target.value)}}
+                            />
+                        </label>
+                    </p>
                     <p><Button type="submit" disabled={!title || !preview || !text} onClick={handleSubmit}>Submit</Button></p>
                 </div>
             </div>
